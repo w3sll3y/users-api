95982555fe21c2a80799073d675b1464726f0df9
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-05-18 18:45:26 -0300

    generate resource company

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, CompanyModule],
   controllers: [AppController],
   providers: [AppService],
 })
