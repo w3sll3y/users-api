c6659a2845571dd6f1e6c7f5fd109cacaf4a5b47
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-05-18 20:21:00 -0300

    implement login, guard and local strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,8 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [],
   controllers: [AuthController],
   providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  validateCompany(email: string, password: string) {
+    throw new Error('Method not implemented.');
+  }
+}
